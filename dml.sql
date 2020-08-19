741984b22aed21ecf65e609782ec69310b03666f
Sunny-NEU <32408063+Sunny-NEU@users.noreply.github.com>
2020-08-19 19:51:25 +0800

    Update dml.sql
    新增

--- dml.sql
+++ dml.sql
@@ -1,7 +1,9 @@
 -- 插入记录  
-
+INSERT INTO student(id,name,age,sex)
+VALUES ('003','王五',26,'男');
 -- 修改记录  
-
+UPDATE student SET id='004',name='赵四',age=30 WHERE id='002';
 --  删除记录  
-
--- 查询记录  
+DELETE FROM student WHERE id='001';  
+-- 查询记录 
+SELECT * FROM student WHERE name='张三';  
